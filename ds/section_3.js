9b500c78b7686fb1bbfd833ee17dd8b74e126121
madthomason <tho16031@byui.edu>
2018-04-28 16:18:59 -0600

    Finished section3

--- ds/section_3.js
+++ ds/section_3.js
@@ -1,13 +1,18 @@
 function loadTraits() {
-
+    var personName = document.getElementById("nameInput").value;
+    var storyHTML = localStorage.getItem(personName);
+    document.getElementById("traitsEditor").value = storyHTML;
 }
 
 function saveTraits() {
-    var traitsBelongTo = document.getElementById("nameInput").value;
+    var personName = document.getElementById("nameInput").value;
     var traitsHTML = document.getElementById("traitsEditor").value;
-    localStorage.setItem(traitsBelongTo, traitsHTML);
+    localStorage.setItem(personName, traitsHTML);
 }
 
 function displayTraits() {
-
+    var personName = document.getElementById("nameInput").value;
+    var traits = document.getElementById("traitsEditor").value;
+    var traitsHTML = personName.concat(" is: ", traits);
+    document.getElementById("displayTraits").innerHTML = traitsHTML;
 }
