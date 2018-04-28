454280508d17f600c0023247cef823e71e851575
madthomason <tho16031@byui.edu>
2018-04-28 16:41:51 -0600

    finished section4

--- ds/ds.js
+++ ds/ds.js
@@ -1,3 +1,23 @@
+//SECTION 5
+function 
+
+//SECTION 4
+function judgeFood() {
+    var food = document.getElementById("foodName").value;
+    var judgement = 'initial';
+    if (food == "gummy worms") {
+        judgement = "YES Please!";
+    }
+    else if(food == "anchovies") {
+        judgement = "NO."
+    }
+    else {
+        judgement = "Maybe. I'll think about it.";
+    }
+    document.getElementById("display").innerHTML = food + "? " + judgement;
+}
+
+//SECTION 3
 function loadTraits() {
     var personName = document.getElementById("nameInput").value;
     var storyHTML = localStorage.getItem(personName);
