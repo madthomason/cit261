2a541d27166cd258e9c14b5d0411ead0e6ac22c5
madthomason <tho16031@byui.edu>
2018-05-17 14:26:11 -0600

    bug fixes

--- ntn/chapter8.js
+++ ntn/chapter8.js
@@ -1,6 +1,6 @@
 //Forms
-//const form = document.forms['search'];
-const form = document.forms.hero;
+/*const form = document.forms['search'];
+
 const input = form['searchInput'];
 
 input.value = 'search here';
@@ -24,48 +24,42 @@ input.addEventListener('blur', function () {
 form.action = 'a/fake.url';
 form.addEventListener('submit', search, false);
 
+
 function search(event) {
     alert(`you searched for: ${input.value}`);
     event.preventDefault();
 }
-
+*/
 
 const form = document.forms['hero'];
 form.addEventListener('submit', makeHero, false);
 
 function makeHero(event) {
     event.preventDefault();
-    const hero
-    {
-    }
-    ;
+    const hero =
+    { };
     hero.name = form.heroName.value;
+//checkboxes
+    form.powers;
+    hero.powers = [];
+    for (let i = 0; i < form.powers.length; i++) {
+        if (form.powers[i].checked) {
+            hero.powers.push(form.powers[i].value);
+        }
+    }
+//radio buttons
+    form.category;
+    hero.category = form.category.value;
+
+    form.age;
+    hero.age = form.age.value;
+//drop down list
+    form.city;
+    hero.city = form.city.value;
+    form.origin;
+    hero.origin = form.origin.value;
+//button types= [submit, reset, button, menu, ect]
     alert(JSON.stringify(hero));
     return hero;
 }
 
-//checkboxes
-form.powers;
-hero.powers = [];
-for (let i = 0; i < form.powers.length; i++) {
-    if (form.powers[i].checked) {
-        hero.powers.push(form.powers[i].value);
-    }
-}
-//radio buttons
-form.category;
-hero.category = form.category.value;
-for (let i = 0; i < form.category.length; i++) {
-    if (form.category[i].)
-}
-//can be done with html checked attribute
-form.type[2].value = 'checked';
-
-form.age;
-hero.age = form.age.value;
-//drop down list
-form.city;
-hero.city = form.city.value;
-form.origin;
-hero.origin = form.origin.value;
-//button types= [submit, reset, button, menu, ect]
