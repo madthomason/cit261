4237ba0adff52695fb3604bd5018692b9c970d3d
madthomason <tho16031@byui.edu>
2018-06-07 09:48:00 -0600

    getting the save to work -not successful yet

--- HOLJournal/hotl.js
+++ HOLJournal/hotl.js
@@ -5,33 +5,38 @@
 //edit pencil on home hide: home, entry->display
 const editPencil = document.getElementById('edit');
 editPencil.addEventListener('click', function () {
-    switchPage(false);});
+    switchPage(false);
+});
 
 //back on entry hide: entry-> display and edit
 const back = document.getElementsByClassName('back');
 back[0].addEventListener('click', function () {
-    switchPage(true);});
+    switchPage(true);
+});
 back[1].addEventListener('click', function () {
     if (back[1].disabled !== true)
-        switchPage(true);});
+        switchPage(true);
+});
 
 //edit display hide: home, entry->display sending in the entry
 const displayToEdit = document.getElementById("display");
 displayToEdit.addEventListener('click', function () {
-    editEntry();});
+    editEntry(true);
+});
 
 const save = document.getElementById("save");
 save.addEventListener('click', saveEntry());
 const cancel = document.getElementById("cancel");
 cancel.addEventListener('click', function () {
-    document.getElementsByClassName("back")[1].style.display= "block";
+    document.getElementsByClassName("back")[1].style.display = "block";
     document.getElementsByClassName("back").disabled = false;
     /*if (element)
         displayEntry(element);
     else*/
-        switchPage(false);
+    switchPage(false);
 
 })
+
 //const edit = document.getElementById('editEntry');
 
 function switchPage(read, element) {
@@ -46,20 +51,21 @@ function switchPage(read, element) {
         //editEntry();
     }
     if (read !== true) {
-        editEntry();
+        editEntry(true);
 
     }
 }
 
-function editEntry() {
+function editEntry(bool) {
     const display = document.getElementById("display");
     const editPage = document.getElementById("editPage");
     display.classList.toggle("hidden");
     editPage.classList.toggle("hidden");
-    document.getElementsByClassName("back")[1].style.display= "none";
-    document.getElementsByClassName("back").disabled = true;
-    document.getElementById("editDate").value = new Date().toISOString().substring(0, 10);
-
+    if (bool === true) {
+        document.getElementsByClassName("back")[1].style.display = "none";
+        document.getElementsByClassName("back").disabled = true;
+        document.getElementById("editDate").value = new Date().toISOString().substring(0, 10);
+    }
 }
 
 function saveEntry() {
@@ -68,7 +74,8 @@ function saveEntry() {
     entry.title = document.getElementById("editTitle").value;
     entry.date = document.getElementById("editDate").value;
     entry.text = document.getElementById("editEntry").value;
-    entry.file = document.getElementById("editFile").value;
+    const file = document.getElementById("editFile").value;
+    entry.file = getBase64Image(file);
 
     var entriesStored = localStorage.getItem("all_entries");
     var allEntries = JSON.parse(entriesStored);
@@ -79,6 +86,7 @@ function saveEntry() {
     entriesStored = JSON.stringify(allEntries);
     localStorage.setItem("all_entries", entriesStored);
     displayEntry(entry);
+    editEntry();
 }
 
 function displayEntry(entry) {
@@ -90,10 +98,25 @@ function displayEntry(entry) {
     }
     else {
         title = entry.title;
-        date = entry.date;
+        date = entry.date.toLocaleString();
     }
     var text = entry.text;
     var files = "";
+
+    /*"<div class="header">
+            <button class="back"><i class="fas fa-arrow-left"></i></button>
+        <h1 id="titleEntry">${title}</h1>
+        <p id="dateEntry">${date}</p>
+            </div>
+            <hr>
+            <p id="textFull">${text}</p>
+        <img src="">
+
+        var preview = document.querySelector('img'); //selects the query named img
+           var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+           var reader  = new FileReader();
+           https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
+        */
 }
 
 function displayHomeList() {
@@ -117,6 +140,20 @@ function displayHomeList() {
     }
 }
 
+/*src =
+https://stackoverflow.com/questions/19183180/how-to-save-an-image-to-localstorage-and-display-it-on-the-next-page*/
+function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
 
 //SECTION 5 -Journaling
 function saveReminder() {
